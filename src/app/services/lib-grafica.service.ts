6598a2675a049333cea2ee7462031fcb643d0365
mandalarotation <mandalarotation@gmail.com>
2020-10-04 23:45:36 -0500

    map

--- src/app/services/lib-grafica.service.ts
+++ src/app/services/lib-grafica.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BackendGraficosService } from './backend-graficos.service'
 
-import data_map from "../../assets/static_examples/map/data.json"
-import layout_map from "../../assets/static_examples/map/layout.json"
+
 import data_line_barranquilla from "../../assets/static_examples/lineas/barranquilla_line/data.json"
 import layout_line_barranquilla from "../../assets/static_examples/lineas/barranquilla_line/layout.json"
 import data_line_bogota from "../../assets/static_examples/lineas/bogota_line/data.json"
@@ -15,6 +14,8 @@ import data_line_medellin from "../../assets/static_examples/lineas/medellin_lin
 import layout_line_medellin from "../../assets/static_examples/lineas/medellin_line/layout.json"
 import data_line_santamarta from "../../assets/static_examples/lineas/santamarta_line/data.json"
 import layout_line_satamarta from "../../assets/static_examples/lineas/santamarta_line/layout.json"
+import data_map from "../../assets/static_examples/lineas/mapas/data.json"
+import layout_map from "../../assets/static_examples/lineas/mapas/layout.json"
 
 
 
@@ -52,6 +53,10 @@ export class LibGraficaService {
             santamarta: { 
                     id: 1,
                     plotp: { data: data_line_santamarta["data"], layout: layout_line_satamarta["layout"]}
+            },
+            map: { 
+                    id: 1,
+                    plotp: { data: data_map["data"], layout: layout_map["layout"]}
             },
             
     }
